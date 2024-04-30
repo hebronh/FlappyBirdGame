ba613f5ef9bc419acbf380716fd29121f1032cd3
Hebron Bekele <105475423+hebronh@users.noreply.github.com>
2024-04-29 23:51:19 -0500

    alpha changes

--- FlappyBird.java
+++ FlappyBird.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.io.IOException;
